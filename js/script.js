e57135cf1fb816abd5647d968c37ea866b27a4ef
Cristian Gardosi <cristian.gardosi97@gmail.com>
2020-10-23 18:04:16 +0200

    ESERCIZIO COMPLETO SENZA BONUS

--- js/script.js
+++ js/script.js
@@ -17,26 +17,61 @@
 // con difficoltà 2 => tra 1 e 50
 
 
+// LV DI DIFFICOLTA'
+// Faccio scegliere all'utente il livello di difficoltà desiderato e setto il while in modo da renderlo continuativo fino a quando l'utente non inserisce un valore valido
 
-// Generazione bombe (16 numeri casuali non doppioni)
+
+
+// Setto le var che mi serviranno in seguito
 var bombList = [];
 var bombNumber = 16;
-// Ciclo while per la generazione delle mie 16 bombe comprese tra 1 e 100
+var min = 1;
+var max = 100;
+
+// GENERAZIONE 16 BOMBE
 while (bombList.length < bombNumber) {
-    var bomb = randomNumber(1, 100);
+    var bomb = bombeRandom(1, 100);
     // If per evitare di avere doppioni
     if (bombList.includes(bomb) === false) {
         bombList.push(bomb);
     }
 }
-
 console.log(bombList);
+
+// RICHIESTA NUMERI ALL'UTENTE (Inizio partita)
+// Var vuota che indica il numero di volta in volta inserito dall'utente
+var inserimentoNumero = '';
+// Numero massimo totale meno il numero delle bombe mi da come risultato il numero massimo di volte in cui il loop può ripetersi, coincide con la vittoria finale del giocatore che non calpesta nessuna mina
+for (var i = 0; i < (max - bombNumber); i++) {
+    // Array che va man mano riempiendosi dei numeri inseriti dall'utente
+    var numeriInseriti = [];
+
+    var inserimentoNumero = parseInt( prompt('Inserisci un numero compreso tra ' + min + ' e ' + max) );
+
+    while (isNaN(inserimentoNumero) || inserimentoNumero < min || inserimentoNumero > max || numeriInseriti.includes(inserimentoNumero)) {
+        inserimentoNumero = parseInt( prompt('ATTENZIONE tentativo fallito! Inserisci un numero compreso tra ' + min + ' e ' + max) );
+    }
+
+    // Imposto attraverso if le situazioni che si vengono a creare in base al numero di volta in volta inserito
+    if (! bombList.includes(inserimentoNumero)) {
+        alert('Nessuna mina qui... Il gioco continua');
+        numeriInseriti.push(inserimentoNumero);
+        inserimentoNumero++;
+    }
+    else {
+        alert('Bomba innescata! Hai perso!'); 
+        break;
+    }
+
+}
+
+
 
 
 // FUNZIONI
 
 // Funzione per la creazione di numeri random entro un determinato intervallo
-function randomNumber(min, max) {
+function bombeRandom(min, max) {
     var random = Math.floor( Math.random() * (max - min + 1) ) + min;
 
     return random;
