ceff51d428bce4b5484aaf542cf6e56f6bef85d5
Cristian Gardosi <cristian.gardosi97@gmail.com>
2020-10-27 10:02:12 +0100

    BUG INFINITE LOOP

--- js/script.js
+++ js/script.js
@@ -1,78 +1,101 @@
-// **********************************************************
-// // CAMPO MINATO!
-// **********************************************************
+// // ************************************************************
+// // CORREZIONE IN CLASSE (VERSIONE CREATA DA Paolo) da lui definita semplificata, ma al tempo stesso verbosa e ricca di molti degli elementi visti fino a qua per ripassare più concetti possibili.
+// // ************************************************************
 
-// Il computer deve generare 16 numeri casuali (bombe) tra 1 e 100.
-// I numeri non possono essere duplicati
-// In seguito deve chiedere all'utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-// L'utente non può inserire più volte lo stesso numero.
-// Se il numero è presente nella lista dei numeri generati (bombe), la partita termina, altrimenti si continua chiedendo all'utente un altro numero.
-// La partita termina quando il giocatore inserisce un numero "vietato" o raggiunge il numero massimo possibile di numeri consentiti.
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l'utente ha inserito un numero consentito.
+// // Setup iniziale variabili utili da settare all'inizio e GENERAZIONE BOMBE
+var numeroMax = 0;
 
-// BONUS: (da fare solo se funziona tutto il resto)
-// all'inizio il software richiede anche una difficoltà all'utente che cambia il range di numeri casuali:
-// con difficoltà 0 => tra 1 e 100
-// con difficoltà 1 =>  tra 1 e 80
-// con difficoltà 2 => tra 1 e 50
+var possibilita = 0; 
+// Lista 16 numeri 'proibiti'
+var listaBombe = [];
+// Numeri ok inseriti dall'utente
+var numeriConsentiti = [];
+// Scelte effettuate volta per volta dall'utente
+var utente = 0; 
 
+// // SCELTA LIVELLO DI DIFFICOLTA'
+var livello = parseInt( prompt('Benvenuto soldato! scegli il livello di difficoltà tra 1 (facile), 2 (medio) e 3 (difficile )').trim() );
 
-// LV DI DIFFICOLTA'
-// Faccio scegliere all'utente il livello di difficoltà desiderato e setto il while in modo da renderlo continuativo fino a quando l'utente non inserisce un valore valido
-
-
-
-// Setto le var che mi serviranno in seguito
-var bombList = [];
-var bombNumber = 16;
-var min = 1;
-var max = 100;
-
-// GENERAZIONE 16 BOMBE
-while (bombList.length < bombNumber) {
-    var bomb = bombeRandom(1, 100);
-    // If per evitare di avere doppioni
-    if (bombList.includes(bomb) === false) {
-        bombList.push(bomb);
-    }
+while ( livello < 1 || livello > 3 || isNaN(livello) ) {
+    livello = parseInt( prompt('Valore inserito non valido! Scegli il livello di difficoltà tra 1 (facile), 2 (medio) e 3 (difficile )').trim() );
 }
-console.log(bombList);
 
-// RICHIESTA NUMERI ALL'UTENTE (Inizio partita)
-// Var vuota che indica il numero di volta in volta inserito dall'utente
-var inserimentoNumero = '';
-// Numero massimo totale meno il numero delle bombe mi da come risultato il numero massimo di volte in cui il loop può ripetersi, coincide con la vittoria finale del giocatore che non calpesta nessuna mina
-for (var i = 0; i < (max - bombNumber); i++) {
-    // Array che va man mano riempiendosi dei numeri inseriti dall'utente
-    var numeriInseriti = [];
-
-    var inserimentoNumero = parseInt( prompt('Inserisci un numero compreso tra ' + min + ' e ' + max) );
-
-    while (isNaN(inserimentoNumero) || inserimentoNumero < min || inserimentoNumero > max || numeriInseriti.includes(inserimentoNumero)) {
-        inserimentoNumero = parseInt( prompt('ATTENZIONE tentativo fallito! Inserisci un numero compreso tra ' + min + ' e ' + max) );
-    }
-
-    // Imposto attraverso if le situazioni che si vengono a creare in base al numero di volta in volta inserito
-    if (! bombList.includes(inserimentoNumero)) {
-        alert('Nessuna mina qui... Il gioco continua');
-        numeriInseriti.push(inserimentoNumero);
-        inserimentoNumero++;
-    }
-    else {
-        alert('Bomba innescata! Hai perso!'); 
+switch (livello) {
+    case 1:
+        numeroMax = 100;
+        possibilita = 84; 
         break;
+
+    case 2:
+        numeroMax = 80;
+        possibilita = 64; 
+        break;
+
+    case 3:
+        numeroMax = 50;
+        possibilita = 34; 
+        break;
+}
+
+// // Generazione bombe, 16 numeri random univoci
+while (listaBombe.length < 16) {
+    // Random, per rendere la creazione più dinamica e scalabile possibile prendo numeroMax dalle classi utility create a inizio documento ed utilizzo la funzione da me creata per la generazione dei numeri random
+    var numeroBomba = numeriRandom(numeroMax);
+    // Push solo se non è gia presente
+    if (listaBombe.includes(numeroBomba === false) ) {
+        listaBombe.push(numeroBomba);
     }
+}
+console.log('LISTA BOMBE: ' + listaBombe);
 
+// LOOP PRINCIPALE per il funzionamento del gioco
+
+// Subito con while setto due condizioni fondamentali: che il numero di numeri inseriti sia inferiore a quello delle possibilità totali (es. 100 - 16 = 84) e che il numero di volta in volta inserito non sia presente nella lista delle 16 bombe
+while ( (numeriConsentiti.length < possibilita) && (listaBombe.includes(utente) === false) ) {
+    // Scelta utente
+    utente = parseInt( prompt('Inserisci compreso tra uno e ' + numeroMax).trim() );
+
+    while ( utente < 1 || utente > numeroMax || isNaN(utente) ) {
+        utente = parseInt( prompt('OPS! Qualcosa non va... Inserisci compreso tra uno e ' + numeroMax).trim() );
+    }
+    console.log('SCELTA UTENTE: ' + utente);
+    // Controllo numero inserito 
+    controlloNumero(utente);
+    // Quanti numeri ha inserito l'utente prima di perdere 
+    if ( numeriConsentiti.length === possibilita ) {
+         alert('HAI VINTO SOLDATO!!! COMPLIMENTI!');
+    }
 }
 
 
-
-
+// ***
 // FUNZIONI
+// ***
 
-// Funzione per la creazione di numeri random entro un determinato intervallo
-function bombeRandom(min, max) {
-    var random = Math.floor( Math.random() * (max - min + 1) ) + min;
-
-    return random;
+// Numeri Random
+function numeriRandom(max) {
+    return Math.floor( Math.random() * max) + 1;
 }
+
+// Controllo numero inserito dall'utente 
+function controlloNumero(num) {
+    // Se num è nella lista bombe
+    if ( listaBombe.includes(num) ) {
+        alert('HAI PERSO! Sei sopravvissuto ' + numeriConsentiti.length + ' turni prima di trovare una bomba, riprova soldato!');
+    }
+    // Se num è gia stato inserito in precedenza 
+    else if ( numeriConsentiti.includes(num) ) {
+        alert('ATTENZIONE! Numero già inserito in precedenza, riprovare con uno nuovo soldato!');
+    }
+    // Se num va bene e si può proseguire nel gioco
+    else if ( numeriConsentiti.includes(num) === false ) {
+        numeriConsentiti.push(num);
+    }
+}
+
+
+
+
+
+
+
